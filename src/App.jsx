e6332491a5feb47754f572cfa588cf648b29e7b1
joao <vctornascimento8@gmail.com>
2023-05-21 18:30:05 -0300

    supabase working

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 
 import Home from '/pages/Home';
-import ShowBooks from '/pages/ShowBooks'
+import ShowBooks from '/pages/ShowBooks';
+import Login from  '/pages/Login';
+import Register from '/pages/Register'
 import {Route, Routes} from '@solidjs/router';
-import BookInfo from '../components/ShowBook/BookInfo';
 
 
 function App() {
   return (
     <Routes>
      <Route path={"/"} component={Home}></Route>
+     <Route path={"/login"} component={Login}></Route>
+     <Route path={"/register"} component={Register}></Route>
      <Route path={"/books/:id"} component={ShowBooks}></Route>
     </Routes>
   );
