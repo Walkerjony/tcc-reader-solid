1fcf8151486dea0731633c5e79cbf63acfaa2956
joao <vctornascimento8@gmail.com>
2023-09-22 16:59:56 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@
 import Home from '/pages/Home';
 import ShowBooks from '/pages/ShowBooks';
 import Login from  '/pages/Login';
-<<<<<<< HEAD
 import Edit from '/pages/Edit';
-=======
->>>>>>> master
 import Register from '/pages/Register'
 import {Route, Routes} from '@solidjs/router';
 
@@ -15,13 +12,9 @@ function App() {
     <Routes>
      <Route path={"/"} component={Home}></Route>
      <Route path={"/login"} component={Login}></Route>
-<<<<<<< HEAD
      <Route path={"/edit"} component={Edit}></Route>
      <Route path={"/register"} component={Register}></Route>
      <Route path={"/edit/:id"} component={Edit}></Route>
-=======
-     <Route path={"/register"} component={Register}></Route>
->>>>>>> master
      <Route path={"/books/:id"} component={ShowBooks}></Route>
     </Routes>
   );
